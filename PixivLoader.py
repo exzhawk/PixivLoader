7014e823106f055bdf5f33c2ab30baf85477bccc
Epix <git@exz.me>
2016-01-12 22:19:26 +0800

    do single thread crawling

--- PixivLoader.py
+++ PixivLoader.py
@@ -1,8 +1,93 @@
+import cPickle
 import os
+from urlparse import urljoin
 
+import requests
 from flask import Flask, send_from_directory, send_file
+from lxml import etree
+
+from account import account
+from settings import *
 
 app = Flask(__name__)
+
+
+class Pixiv:
+    cookies_filename = os.path.join(app.root_path, 'cookies.pkl')
+    base_urls = {
+        'following_illust': 'http://www.pixiv.net/bookmark_new_illust.php?p=%s',
+        'login': 'https://www.secure.pixiv.net/login.php',
+        'test_login': 'http://www.pixiv.net/stacc/?mode=unify',
+        'home': 'http://www.pixiv.net/',
+    }
+    session = requests.Session()
+    session.proxies = PROXIES
+    session.headers = HEADERS
+    session.verify = False
+    is_login = False
+
+    def __init__(self):
+        try:
+            self.session.cookies = cPickle.load(open(self.cookies_filename, 'rb'))
+        except IOError:
+            pass
+
+    def login(self):
+        self.test_login()
+        if self.is_login is False:
+            self.do_login()
+        print(self.is_login)
+
+    def test_login(self):
+        response = self.get(url=self.base_urls['test_login'], allow_redirects=False)
+        if response.status_code == 200:
+            self.is_login = True
+
+    def do_login(self):
+        account_extra = ('mode', 'login'), ('skip', '1')
+        response = self.post(url=self.base_urls['login'], data=dict(account_extra, **account))
+        if response.url == u'http://www.pixiv.net/':
+            self.is_login = True
+            cPickle.dump(self.session.cookies, open(self.cookies_filename, 'wb'))
+
+    def get(self, url, **kwargs):
+        return self.session.get(url=url, **kwargs)
+
+    def post(self, url, data=None, **kwargs):
+        return self.session.post(url=url, data=data, **kwargs)
+
+    def get_following(self, page_number):
+        response = self.get(self.base_urls['following_illust'] % page_number)
+        result = list(self.parse_list(response))
+        return result
+
+    def parse_list(self, response):
+        tree = etree.HTML(response.text)
+        illusts = tree.xpath('//li[@class="image-item "]')
+        for illust in illusts:
+            illust_url = illust.xpath('./a/@href')[0]
+            # cache thumbnails
+            # todo change url to manga mode if multi
+            illust_response = self.get(urljoin(self.base_urls['home'], illust_url), headers={'Referer': response.url})
+            yield self.parse_illust(illust_response)
+
+    def parse_illust(self, response):
+        tree = etree.HTML(response.text)
+        # todo parse single illust page and download pic
+        pass
+
+    def parse_manga(self, response):
+        tree = etree.HTML(response.text)
+        # todo parse single illust page and download pic
+        pass
+
+
+pixiv = Pixiv()
+
+
+@app.route('/get_following/<int:page_number>')
+def get_following(page_number):
+    return pixiv.get_following(page_number)
 
 
 @app.route('/')
@@ -16,5 +101,6 @@ def static_html(path):
 
 
 if __name__ == '__main__':
-    app.debug = True
+    pixiv.login()
+    # app.debug = True
     app.run()
---
+++ settings.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+# Author: Epix
+PROXIES = {
+    'http': 'http://127.0.0.1:8888',
+    'https': 'http://127.0.0.1:8888',
+}
+HEADERS = {
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
+}
+CACHE_DIR = ''
