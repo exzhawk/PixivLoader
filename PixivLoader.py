659dcc83e2a8e7dc732c1c3abb8838e8d163faf1
Epix <git@exz.me>
2016-01-19 20:22:57 +0800

    workable cache and result return

--- PixivLoader.py
+++ PixivLoader.py
@@ -1,11 +1,12 @@
+import json
 import os
 import pickle
-from urllib.parse import urljoin
+from urllib.parse import urljoin, quote
 
-import requests
 import tornado.ioloop
 import tornado.web
 from lxml import etree
+from requests_futures.sessions import FuturesSession
 
 from account import account
 from settings import *
@@ -19,7 +20,8 @@ class Pixiv:
         'test_login': 'http://www.pixiv.net/stacc/?mode=unify',
         'home': 'http://www.pixiv.net/',
     }
-    session = requests.Session()
+    # session = requests.Session()
+    session = FuturesSession(max_workers=10)
     session.proxies = PROXIES
     session.headers = HEADERS
     session.verify = False
@@ -50,18 +52,30 @@ class Pixiv:
             pickle.dump(self.session.cookies, open(self.cookies_filename, 'wb'))
 
     def get(self, url, **kwargs):
-        return self.session.get(url=url, **kwargs)
+        return self.session.get(url=url, **kwargs).result()
 
     def post(self, url, data=None, **kwargs):
-        return self.session.post(url=url, data=data, **kwargs)
+        return self.session.post(url=url, data=data, **kwargs).result()
 
     def get_following(self, page_number):
         response = self.get(self.base_urls['following_illust'] % page_number)
         result = list(self.parse_list(response))
         return result
 
-    def get_pic(self, url, referer):
-        pass
+    def cache_pic(self, url, referer):
+        file_name = quote(url, safe='')
+        file_path = os.path.join(CACHE_DIR, file_name)
+        if os.path.isfile(file_path):
+            pass
+        else:
+            self.session.get(url, headers={'Referer': referer}, background_callback=self.save_pic)
+
+    def save_pic(self, session, response):
+        url = response.url
+        file_name = quote(url, safe='')
+        file_path = os.path.join(CACHE_DIR, file_name)
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
 
     def parse_list(self, response):
         tree = etree.HTML(response.text)
@@ -73,29 +87,40 @@ class Pixiv:
             illust_author_id = illust_author.xpath('./@data-user_id')[0]
             illust_title = illust.xpath('./a/h1[@class="title"]/text()')[0]
             illust_thumbnail_url = illust.xpath('./a/div[@class="_layout-thumbnail"]/img[@class="_thumbnail"]/@src')[0]
-            # todo cache thumbnails
-            is_multi = 'multiple' in illust.xpath('./a[0]/@class')[0]
+            self.cache_pic(illust_thumbnail_url, response.url)
+            is_multi = 'multiple' in illust.xpath('./a[1]/@class')[0]
             if is_multi:
                 illust_url = illust_url.replace('medium', 'manga')
             illust_response = self.get(urljoin(self.base_urls['home'], illust_url), headers={'Referer': response.url})
+            meta = {'url': illust_url,
+                    'title': illust_title,
+                    'author_name': illust_author_name,
+                    'author_id': illust_author_id,
+                    'thumbnail': illust_thumbnail_url,
+                    'is_multi': is_multi}
             if is_multi:
-                yield self.parse_manga(illust_response)
+                yield self.parse_manga(illust_response, meta)
             else:
-                yield self.parse_illust(illust_response)
+                yield self.parse_illust(illust_response, meta)
 
-    def parse_illust(self, response):
+    def parse_illust(self, response, meta):
+        d = dict(meta)
         tree = etree.HTML(response.text)
         img_src = tree.xpath('//div[starts-with(@class,"_illust_modal")]/div[@class="wrapper"]/img/@data-src')[0]
-        # todo download pic and return
-        pass
+        self.cache_pic(img_src, response.url)
+        d['img'] = [img_src]
+        return d
 
-    def parse_manga(self, response):
+    def parse_manga(self, response, meta):
+        d = dict(meta)
+        d['img'] = []
         tree = etree.HTML(response.text)
         pics = tree.xpath('//div[@class="item-container"]')
         for pic in pics:
             img_src = pic.xpath('./img/@data-src')[0]
-        # todo download pic and return
-        pass
+            self.cache_pic(img_src, response.url)
+            d['img'].append(img_src)
+        return d
 
 
 pixiv = Pixiv()
@@ -103,7 +128,8 @@ pixiv = Pixiv()
 
 class GetFollowingHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
-        return pixiv.get_following(args[0])
+        result = json.dumps(pixiv.get_following(args[0]))
+        self.write(result)
 
 
 if __name__ == '__main__':
